18003d1cf26a31ebc9d858e14970695f00b66581
st-lana <st.lana@hotmail.com>
2021-12-14 22:27:17 +0300

    File BonusServiceTest.java updated

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -30,4 +30,30 @@ class BonusServiceTest {
         assertEquals(expected, actual);
 
     }
+
+    @Test
+    void shouldCalculateUnregisteredAndBonusUnderLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateUnregisteredAndBonusOverLimit() {
+        BonusService service = new BonusService();
+
+        long amount = 100_000_60;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
 }
